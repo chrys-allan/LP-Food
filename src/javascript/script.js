a4ecc0d691b48eb07bfcf6724757a6b5b4031200
Chrystopher Allan <chrystopher_allan@yahoo.com.br>
2024-08-14 23:45:21 -0300

    second commit

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -11,13 +11,13 @@ $(document).ready(function(){
        const header = $('header'); 
        const scrollPosition = $(window).scrollTop() - header.outerHeight();
        
-       let activeSectionIndex = 0;
+      /*  let activeSectionIndex = 0; */
 
        if (scrollPosition <= 0) {
             header.css('box-shadow', 'none');
        } 
        else {
-            header.css('bow-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
         }
 
         sections.each(function(i) {
